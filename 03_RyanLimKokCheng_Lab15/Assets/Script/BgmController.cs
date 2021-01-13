d2ef69d7c3c9fc7fbfd8ec341646f938f44a941f
SumanaiEugen <ryanlim943@gmail.com>
2021-01-14 00:56:19 +0800

    Main

---
+++ 03_RyanLimKokCheng_Lab15/Assets/Script/BgmController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgmController : MonoBehaviour
+{
+    public AudioClip[] AudioClipsArr;
+
+    private AudioSource audioSource;
+
+    float vol = 0.5f;
+    float dvol = 1f;
+    float volrate = 0.15f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        { 
+            int rand = Random.Range(0, AudioClipsArr.Length);
+            audioSource.PlayOneShot(AudioClipsArr[rand]);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            vol = vol + (volrate * Time.deltaTime);
+            audioSource.volume = vol;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            vol = vol - (volrate * Time.deltaTime);
+            audioSource.volume = vol;
+        }
+    }
+}
---
+++ 03_RyanLimKokCheng_Lab15/Assets/Script/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject Redtext;
+    public Rigidbody PlayerRb;
+
+    private AudioSource audioSource;
+    public AudioClip[] audioClipArray;
+
+    private int jumpcounter;
+    int JumpCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.Space) && JumpCount == 0)
+        {
+            PlayerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+
+            JumpCount = 1;
+
+            //Text Counter
+            jumpcounter++;
+            Redtext.GetComponent<Text>().text = "Jump Count : " + jumpcounter;
+
+            //Random Audio plays on jump
+            int rand = Random.Range(0, audioClipArray.Length);
+            audioSource.PlayOneShot(audioClipArray[rand]);
+        }
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Grounded"))
+        {
+            JumpCount = 0;
+        }
+    }
+}
